e7ad93ae5c1621dd558e386f1e236490473aa6e0
Pale1999 <zainbintauqeer@gmail.com>
2022-07-01 19:29:21 +0500

    Um, hi

---
+++ MathFunctions.py
@@ -0,0 +1,11 @@
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def sub(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
